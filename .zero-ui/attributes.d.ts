996ea6d16cbae434ad389ef0f4a9fd7240f63f88
Austin1serb <serbaustin@gmail.com>
2025-07-18 21:31:42 -0700

    made approach tabs render-less

--- .zero-ui/attributes.d.ts
+++ .zero-ui/attributes.d.ts
@@ -1,5 +1,5 @@
 /* AUTO-GENERATED - DO NOT EDIT */
 export declare const bodyAttributes: {
-  "data-phase": "1" | "2" | "3" | "4" | "5"
-  "data-reveal": "false"
-}
+  "data-phase": "1" | "2" | "3" | "4" | "5";
+  "data-reveal": "false";
+};
--- .zero-ui/attributes.js
+++ .zero-ui/attributes.js
@@ -1,5 +1,5 @@
 /* AUTO-GENERATED - DO NOT EDIT */
 export const bodyAttributes = {
   "data-phase": "1",
-  "data-reveal": "false",
-}
+  "data-reveal": "false"
+};
