e3d261edbbaa993445fc1e124dae9e25b597870a
Austin1serb <serbaustin@gmail.com>
2025-08-26 08:13:07 -0700

    update gitignore

--- .zero-ui/attributes.d.ts
+++ .zero-ui/attributes.d.ts
@@ -1,6 +1,5 @@
 /* AUTO-GENERATED - DO NOT EDIT */
 export declare const bodyAttributes: {
-  "data-faq": string;
   "data-mobile-menu": string;
   "data-phase": "1" | "2" | "3" | "4" | "5";
   "data-reveal": "false";
